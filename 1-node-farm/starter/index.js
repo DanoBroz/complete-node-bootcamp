274c3d4377802a8520815785516b2ee62fb06254
Daniel Brož <danobroz66@gmail.com>
2022-09-19 18:58:35 +0200

    add error handle for first file

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -12,6 +12,8 @@ const fs = require('fs')
 
 //Non-blocking asynchrounous way
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('Error! 💥')
+
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
         console.log(data2)
         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
