169986c99aa0147e3da349843b159004ac182702
Daniel Brož <danobroz66@gmail.com>
2022-09-19 18:53:44 +0200

    read and write file with async (callback-hell)

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,10 +1,30 @@
 const fs = require('fs')
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(textIn)
+//Blocking synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn)
 
-const textOut = `This is what we know about the avocado: ${textIn}. Created on ${Date.now()}`
+// const textOut = `This is what we know about the avocado: ${textIn}. Created on ${Date.now()}`
 
-fs.writeFileSync('./txt/output.txt', textOut)
+// fs.writeFileSync('./txt/output.txt', textOut)
 
-console.log('File has been written')
+// console.log('File has been written')
+
+//Non-blocking asynchrounous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2)
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3)
+
+            fs.writeFile(
+                './txt/final.txt',
+                `${data2}\n${data3}`,
+                'utf-8',
+                (err) => {
+                    console.log('Yout file has been written 😁')
+                }
+            )
+        })
+    })
+})
