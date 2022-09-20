92a975408321d81d8a2a89f15b36e73f588a7584
Daniel Brož <danobroz66@gmail.com>
2022-09-20 08:23:35 +0200

    routing essentials

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
 
 //#region Blocking synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
@@ -35,8 +36,30 @@ const http = require('http')
 //#endregion
 
 //#region first server creation
+// const server = http.createServer((req, res) => {
+//     res.end('Hello from the server!')
+// })
+
+// server.listen(8000, '127.0.0.1', () => {
+//     console.log('Listening to request on port 8000...')
+// })
+//#endregion
+
+//#region routing essentials
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server!')
+    const pathName = req.url
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the overview')
+    } else if (pathName === '/product') {
+        res.end('This is the product')
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello world',
+        })
+        res.end('<h1>404 the page not found</h1>')
+    }
 })
 
 server.listen(8000, '127.0.0.1', () => {
