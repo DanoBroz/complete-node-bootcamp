693f6ecc95117afbeac0d19a8f6aaf14ac8618a4
Daniel Brož <danobroz66@gmail.com>
2022-09-27 18:01:41 +0200

    add query middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -76,6 +76,10 @@ const tourSchema = new mongoose.Schema(
             select: false,
         },
         startDates: [Date],
+        secretTour: {
+            type: Boolean,
+            default: false,
+        },
     },
     {
         toJSON: { virtuals: true },
@@ -89,6 +93,7 @@ tourSchema
         return this.duration / 7
     })
 
+// Document middleware
 tourSchema.pre('save', function (next) {
     this.slug = slugify(this.name, {
         lower: true,
@@ -105,6 +110,24 @@ tourSchema.pre('save', function (next) {
 //     console.log(doc)
 //     next()
 // })
+
+// Query middleware
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } })
+
+    this.start = Date.now()
+    next()
+})
+
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(
+        `Query took ${
+            Date.now() - this.start
+        } miliseconds!`
+    )
+    console.log(docs)
+    next()
+})
 
 const Tour = mongoose.model('Tour', tourSchema)
 
